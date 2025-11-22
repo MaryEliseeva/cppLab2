447c58661578743b6d907d82e92e824c36bf8a49
MaryEliseeva <marymaryeliseeva@gmail.com>
2025-11-22 18:55:50 +0300

    tests

--- lab2/Subject.h
+++ lab2/Subject.h
@@ -2,8 +2,24 @@
 #define SUBJECT_H
 
 struct Subject {
-    int f3(int arg1, int arg2) {
-        return arg1 + arg2;
+    int f0() {
+        return 42; 
+    }
+
+    int f1(int a) {
+        return a * 2; 
+    }
+
+    int f2(int a, int b) {
+        return a + b; 
+    }
+
+    int f3(int a, int b, int c) {
+        return a + b + c; 
+    }
+
+    int f4(int a, int b, int c, int d) {
+        return a * b - c + d; 
     }
 };
 
--- lab2/main.cpp
+++ lab2/main.cpp
@@ -4,17 +4,45 @@
 #include <iostream>
 #include <map>
 
+void test_f0(Engine& engine, Subject& subj) {
+    Wrapper<Subject, int> w0(&subj, &Subject::f0, {});
+    engine.register_command(&w0, "cmd0");
+    std::cout << "f0: " << engine.execute("cmd0", {}) << std::endl;
+}
+
+void test_f1(Engine& engine, Subject& subj) {
+    Wrapper<Subject, int, int> w1(&subj, &Subject::f1, { "a" });
+    engine.register_command(&w1, "cmd1");
+    std::cout << "f1: " << engine.execute("cmd1", { {"a",5} }) << std::endl;
+}
+
+void test_f2(Engine& engine, Subject& subj) {
+    Wrapper<Subject, int, int, int> w2(&subj, &Subject::f2, { "a","b" });
+    engine.register_command(&w2, "cmd2");
+    std::cout << "f2: " << engine.execute("cmd2", { {"a",3},{"b",7} }) << std::endl;
+}
+
+void test_f3(Engine& engine, Subject& subj) {
+    Wrapper<Subject, int, int, int, int> w3(&subj, &Subject::f3, { "a","b","c" });
+    engine.register_command(&w3, "cmd3");
+    std::cout << "f3: " << engine.execute("cmd3", { {"a",1},{"b",2},{"c",3} }) << std::endl;
+}
+
+void test_f4(Engine& engine, Subject& subj) {
+    Wrapper<Subject, int, int, int, int, int> w4(&subj, &Subject::f4, { "a","b","c","d" });
+    engine.register_command(&w4, "cmd4");
+    std::cout << "f4: " << engine.execute("cmd4", { {"a",2},{"b",3},{"c",1},{"d",4} }) << std::endl;
+}
+
 int main() {
     Subject subj;
-
-    Wrapper<Subject, int, int, int> wrapper(&subj, &Subject::f3, { "arg1", "arg2" });
-
     Engine engine;
-    engine.register_command(&wrapper, "command1");
 
-    std::map<std::string, int> args{ {"arg1", 4}, {"arg2", 5} };
-
-    std::cout << engine.execute("command1", args) << std::endl; // 9
+    test_f0(engine, subj);
+    test_f1(engine, subj);
+    test_f2(engine, subj);
+    test_f3(engine, subj);
+    test_f4(engine, subj);
 
     return 0;
 }
