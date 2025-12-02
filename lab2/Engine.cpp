cb317315239635a6dbf4b2807416146a77579a73
MaryEliseeva <marymaryeliseeva@gmail.com>
2025-12-02 23:04:58 +0300

    correct pimpl

--- lab2/Engine.cpp
+++ lab2/Engine.cpp
@@ -1,11 +1,23 @@
 #include "Engine.h"
+#include <unordered_map>
 #include <stdexcept>
 
-Engine::Engine() : impl_(std::make_unique<Impl>()) {}
+struct Engine::Impl {
+    std::unordered_map<std::string, std::function<int(const std::map<std::string, int>&)>> commands;
+};
+
+Engine::Engine(): impl_(std::make_unique<Impl>()){}
+
 Engine::~Engine() = default;
 
-int Engine::execute(const std::string& name, const std::map<std::string, int>& args) {
+void Engine::register_command(const std::string& name, std::function<int(const std::map<std::string, int>&)> fn) {
+    impl_->commands[name] = std::move(fn);
+}
+
+int Engine::execute(const std::string& name,
+    const std::map<std::string, int>& args) {
     auto it = impl_->commands.find(name);
-    if (it != impl_->commands.end()) return it->second(args);
+    if (it != impl_->commands.end())
+        return it->second(args);
     throw std::runtime_error("Command not found");
 }
--- lab2/Engine.h
+++ lab2/Engine.h
@@ -2,30 +2,28 @@
 #define ENGINE_H
 
 #include <map>
+#include <memory>
 #include <string>
-#include <unordered_map>
 #include <functional>
-#include <memory> 
 
 class Engine {
 public:
     Engine();
     ~Engine();
 
-    template<typename WrapperType>
-    void register_command(WrapperType* wrapper, const std::string& name) {
-        impl_->commands[name] = [wrapper](const std::map<std::string, int>& args) {
-            return (*wrapper)(args);
-            };
-    }
+    void register_command( const std::string& name, std::function<int(const std::map<std::string, int>&)> fn);
 
     int execute(const std::string& name, const std::map<std::string, int>& args);
 
-private:
-    struct Impl {
-        std::unordered_map<std::string, std::function<int(const std::map<std::string, int>&)>> commands;
-    };
+    template<typename WrapperType>
+    void register_command(WrapperType* wrapper, const std::string& name) {
+        register_command(name, [wrapper](const auto& args) {
+            return (*wrapper)(args);
+            });
+    }
 
+private:
+    struct Impl;
     std::unique_ptr<Impl> impl_;
 };
 
